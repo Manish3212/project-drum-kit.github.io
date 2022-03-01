0f285148ece8b263ceef95d30bb16d862b81c20c
Manish3212 <92084691+Manish3212@users.noreply.github.com>
2022-03-01 11:47:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+//click
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    makesound(this.innerHTML);
+    buttonanimation(this.innerHTML)
+});
+}
+
+//keys
+document.addEventListener("keydown",function(event){
+  makesound(event.key)
+  buttonanimation(event.key)
+})
+     
+function makesound(key){
+  switch (key) {
+    case "w":
+    var tom= new Audio("sounds/crash.mp3")
+    tom.play();
+    break;
+  
+    case "a":
+    var kick= new Audio("sounds/kick-bass.mp3")
+    kick.play();
+    break;
+
+     case "s":
+        var audio= new Audio("sounds/snare.mp3")
+        audio.play();
+        break;
+
+      case "d":
+      var audio= new Audio("sounds/tom-1.mp3")
+      audio.play();
+      break;
+
+    case "j":
+        var audio= new Audio("sounds/tom-2.mp3")
+        audio.play();
+        break;
+
+    case "k":
+    var audio= new Audio("sounds/tom-3.mp3")
+     audio.play();
+     break;
+    
+     case "l":
+    var audio= new Audio("sounds/tom-4.mp3")
+     audio.play();
+     break;
+    
+     default:console.log(this.innerHTML);
+}
+}
+function buttonanimation(currentkey){
+  var activebutton= document.querySelector("."+currentkey)
+  activebutton.classList.add("pressed")
+  setTimeout(function(){
+    activebutton.classList.remove("pressed")
+  },100)
+
+}
